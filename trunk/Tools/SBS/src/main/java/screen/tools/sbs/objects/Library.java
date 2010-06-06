4212fadf00fd43d8b718ad3c05226a9b1ec4682d
thoratou <thoratou@df6f4744-0210-11df-9fb0-db4daf2f83fa>
2010-06-06 08:38:16 +0000

    missing class in SBS + updated linux config

---
+++ trunk/Tools/SBS/src/main/java/screen/tools/sbs/objects/Library.java
@@ -0,0 +1,29 @@
+package screen.tools.sbs.objects;
+
+import screen.tools.sbs.utils.FieldString;
+
+public class Library {
+	private FieldString name;
+	private FieldString version;
+	
+	public Library(){
+		name = new 	FieldString();
+		version = new 	FieldString();
+	}
+
+	public void setName(FieldString name) {
+		this.name = name;
+	}
+
+	public FieldString getName() {
+		return name;
+	}
+
+	public void setVersion(FieldString version) {
+		this.version = version;
+	}
+
+	public FieldString getVersion() {
+		return version;
+	}
+}
