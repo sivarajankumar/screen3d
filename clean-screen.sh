075d3b84b74128f22c416d31212b752258268efb
thoratou <thoratou@df6f4744-0210-11df-9fb0-db4daf2f83fa>
2010-11-06 15:58:30 +0000

    changed scripts to compile under linux

--- clean-screen.sh
+++ clean-screen.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-$SBS_ROOT/sbs.sh Screen/Utils clean -e linux -e screen-linux -e thoratou-linux -d
-$SBS_ROOT/sbs.sh Screen/Math clean -e linux -e screen-linux -e thoratou-linux -d
-$SBS_ROOT/sbs.sh Screen/Memory clean -e linux -e screen-linux -e thoratou-linux -d -b
-$SBS_ROOT/sbs.sh Screen/Fields clean -e linux -e screen-linux -e thoratou-linux -d
-$SBS_ROOT/sbs.sh Screen/XML clean -e linux -e screen-linux -e thoratou-linux -d
-$SBS_ROOT/sbs.sh Screen/Main clean -i sbs-linux.xml -e linux -e screen-linux -e thoratou-linux -d -b
-$SBS_ROOT/sbs.sh Screen/OpenGL clean -i sbs-linux.xml -e linux -e screen-linux -e thoratou-linux -d
-$SBS_ROOT/sbs.sh Demo/SFML clean -e linux -e screen-linux -e thoratou-linux -d
+$SBS_ROOT/sbs.sh clean Screen/Utils
+$SBS_ROOT/sbs.sh clean Screen/Math
+$SBS_ROOT/sbs.sh clean Screen/Memory -b
+$SBS_ROOT/sbs.sh clean Screen/Fields
+$SBS_ROOT/sbs.sh clean Screen/XML
+$SBS_ROOT/sbs.sh clean Screen/Main -i sbs-linux.xml -b
+$SBS_ROOT/sbs.sh clean Screen/OpenGL -i sbs-linux.xml
+$SBS_ROOT/sbs.sh clean Demo/SFML
 
--- compil-screen-release.sh
+++ compil-screen-release.sh
@@ -1,52 +1,12 @@
 #!/bin/sh
 
-$SBS_ROOT/sbs.sh Screen/Utils generate -e linux -e screen-linux -e thoratou-linux
-$SBS_ROOT/sbs.sh Screen/Math generate -e linux -e screen-linux -e thoratou-linux
-$SBS_ROOT/sbs.sh Screen/Memory generate -e linux -e screen-linux -e thoratou-linux -b
-$SBS_ROOT/sbs.sh Screen/Fields generate -e linux -e screen-linux -e thoratou-linux
-$SBS_ROOT/sbs.sh Screen/XML generate -e linux -e screen-linux -e thoratou-linux
-$SBS_ROOT/sbs.sh Screen/Main generate -i sbs-linux.xml -e linux -e screen-linux -e thoratou-linux -b
-$SBS_ROOT/sbs.sh Screen/OpenGL generate -i sbs-linux.xml -e linux -e screen-linux -e thoratou-linux
-$SBS_ROOT/sbs.sh Demo/SFML generate -e linux -e screen-linux -e thoratou-linux
-
-cd Screen/Utils
-make -j 4 all
-cd ../..
-
-cd Screen/Math
-make -j 4 all
-cd ../..
-
-cd Screen/Memory
-make -j 4 all
-cd ../..
-
-cd Screen/Memory/test
-make -j 4 all
-cd ../../..
-
-cd Screen/Fields
-make -j 4 all
-cd ../..
-
-cd Screen/XML
-make -j 4 all
-cd ../..
-
-cd Screen/Main
-make -j 4 all
-cd ../..
-
-cd Screen/Main/test
-make -j 4 all
-cd ../../..
-
-cd Screen/OpenGL
-make -j 4 all
-cd ../..
-
-cd Demo/SFML
-make -j 4 all
-cd ../..
+$SBS_ROOT/sbs.sh build Screen/Utils
+$SBS_ROOT/sbs.sh build Screen/Math
+$SBS_ROOT/sbs.sh build Screen/Memory -b
+$SBS_ROOT/sbs.sh build Screen/Fields
+$SBS_ROOT/sbs.sh build Screen/XML
+$SBS_ROOT/sbs.sh build Screen/Main -i sbs-linux.xml -b
+$SBS_ROOT/sbs.sh build Screen/OpenGL -i sbs-linux.xml
+$SBS_ROOT/sbs.sh build Demo/SFML
 
 ln -s ~/.sbs/Screen/OpenGL/0.1.0/lib/Linux/Release/libScreenOpenGL.so ~/.sbs/Screen/Main/0.1.0/lib/Linux/Release/ScreenOpenGL.so 
