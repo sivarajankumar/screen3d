a2bc322769564b69c7e3542f2926530475fb5489
thoratou <thoratou@df6f4744-0210-11df-9fb0-db4daf2f83fa>
2011-01-07 20:08:38 +0000

    windows scripts to generate env files

---
+++ generate-screen-debug.bat
@@ -0,0 +1,8 @@
+call %SBS_ROOT%/sbs.bat generate Screen/Utils -d
+call %SBS_ROOT%/sbs.bat generate Screen/Math -d
+call %SBS_ROOT%/sbs.bat generate Screen/Memory -b -d
+call %SBS_ROOT%/sbs.bat generate Screen/Fields -d
+call %SBS_ROOT%/sbs.bat generate Screen/XML -d
+call %SBS_ROOT%/sbs.bat generate Screen/Main -b -d
+call %SBS_ROOT%/sbs.bat generate Screen/OpenGL -d
+call %SBS_ROOT%/sbs.bat generate Demo/SFML -d
