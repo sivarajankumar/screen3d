d367a809b41ce1fde641e0eccddf846fd0e7c025
thoratou <thoratou@df6f4744-0210-11df-9fb0-db4daf2f83fa>
2011-03-13 13:59:07 +0000

    fix Linux release build script

--- trunk/compil-screen-release.sh
+++ trunk/compil-screen-release.sh
@@ -3,9 +3,9 @@
 $SBS_ROOT/sbs.sh build Screen/Utils
 $SBS_ROOT/sbs.sh build Screen/Math
 $SBS_ROOT/sbs.sh build Screen/Memory -b
-$SBS_ROOT/sbs.sh build Screen/Fields
-$SBS_ROOT/sbs.sh build Screen/XML
-$SBS_ROOT/sbs.sh build Screen/Main -i sbs-linux.xml -b
+#$SBS_ROOT/sbs.sh build Screen/Fields
+#$SBS_ROOT/sbs.sh build Screen/XML
+$SBS_ROOT/sbs.sh build Screen/Main -b
 $SBS_ROOT/sbs.sh build Screen/OpenGL -i sbs-linux.xml
 $SBS_ROOT/sbs.sh build Demo/SFML
 
