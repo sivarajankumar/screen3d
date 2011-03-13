fc5586fef621dfb7f5376958499ba2e237fbeaa5
thoratou <thoratou@df6f4744-0210-11df-9fb0-db4daf2f83fa>
2011-03-13 13:51:10 +0000

    fix Linux deps + merge windows and linux sbs.xml for Screen/Main

--- compil-screen-debug.sh
+++ compil-screen-debug.sh
@@ -3,9 +3,9 @@
 $SBS_ROOT/sbs.sh build Screen/Utils -d
 $SBS_ROOT/sbs.sh build Screen/Math -d
 $SBS_ROOT/sbs.sh build Screen/Memory -b -d
-$SBS_ROOT/sbs.sh build Screen/Fields -d
-$SBS_ROOT/sbs.sh build Screen/XML -d
-$SBS_ROOT/sbs.sh build Screen/Main -i sbs-linux.xml -b -d
+#$SBS_ROOT/sbs.sh build Screen/Fields -d
+#$SBS_ROOT/sbs.sh build Screen/XML -d
+$SBS_ROOT/sbs.sh build Screen/Main -b -d
 $SBS_ROOT/sbs.sh build Screen/OpenGL -i sbs-linux.xml -d
 $SBS_ROOT/sbs.sh build Demo/SFML -d
 
