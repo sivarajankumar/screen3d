e03dc11978f7113a9b20642fb74e0ba2338540af
thoratou <thoratou@df6f4744-0210-11df-9fb0-db4daf2f83fa>
2011-07-01 19:41:14 +0000

    new Windows clean script

---
+++ clean-screen.bat
@@ -0,0 +1,8 @@
+call "%SBS_ROOT%/sbs.bat" clean Screen/Utils
+call "%SBS_ROOT%/sbs.bat" clean Screen/Math
+call "%SBS_ROOT%/sbs.bat" clean Screen/Memory -b
+call "%SBS_ROOT%/sbs.bat" clean Screen/Fields
+call "%SBS_ROOT%/sbs.bat" clean Screen/XML
+call "%SBS_ROOT%/sbs.bat" clean Screen/Main -b
+call "%SBS_ROOT%/sbs.bat" clean Screen/OpenGL
+call "%SBS_ROOT%/sbs.bat" clean Demo/SFML
