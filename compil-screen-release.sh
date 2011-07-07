4ecfa5fa7dbf018f3c8d1268d8c6fefddb573bc9
thoratou <thoratou@df6f4744-0210-11df-9fb0-db4daf2f83fa>
2011-07-07 23:51:44 +0000

    changed SBS script to use SBS_HOME instead of SBS_ROOT

--- compil-screen-release.sh
+++ compil-screen-release.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
-$SBS_ROOT/sbs.sh build Screen/Utils
-$SBS_ROOT/sbs.sh build Screen/Math
-$SBS_ROOT/sbs.sh build Screen/Memory -b
-#$SBS_ROOT/sbs.sh build Screen/Fields
-#$SBS_ROOT/sbs.sh build Screen/XML
-$SBS_ROOT/sbs.sh build Screen/Main -b
-$SBS_ROOT/sbs.sh build Screen/OpenGL -i sbs-linux.xml
-$SBS_ROOT/sbs.sh build Demo/SFML
-
-ln -s ~/.sbs/Screen/OpenGL/0.1.0/lib/Linux/Release/libScreenOpenGL.so ~/.sbs/Screen/Main/0.1.0/lib/Linux/Release/ScreenOpenGL.so 
+$SBS_HOME/sbs.sh build Screen/Utils
+$SBS_HOME/sbs.sh build Screen/Math
+$SBS_HOME/sbs.sh build Screen/Memory -b
+#$SBS_HOME/sbs.sh build Screen/Fields
+#$SBS_HOME/sbs.sh build Screen/XML
+$SBS_HOME/sbs.sh build Screen/Main -b
+$SBS_HOME/sbs.sh build Screen/OpenGL -i sbs-linux.xml
+$SBS_HOME/sbs.sh build Demo/SFML
