bdee8787febb203c745410b405e9bb1bb52f7572
thoratou <thoratou@df6f4744-0210-11df-9fb0-db4daf2f83fa>
2012-01-15 13:31:23 +0000

    Doxygen and code rules for profiler reporters

--- trunk/Screen/Utils/include/screen/utils/ProfilerReporter.h
+++ trunk/Screen/Utils/include/screen/utils/ProfilerReporter.h
@@ -18,6 +18,12 @@
  * Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA, or go to   *
  * http://www.gnu.org/copyleft/lesser.txt.                                   *
  *****************************************************************************/
+/**
+ * \file screen/utils/ProfilerReporter.h
+ * \brief Screen/Utils profile data output handling header file
+ * \author
+ *
+ */
 
 #ifndef SCREEN_PROFILER_REPORTER_H
 #define SCREEN_PROFILER_REPORTER_H
@@ -25,28 +31,70 @@
 #include <screen/utils/Export.h>
 #include <fstream>
 
+/**
+ * Namespace for all screen classes
+ */
 namespace screen {
+    /**
+     * Namespace for all utility classes
+     */
     namespace utils {
     	struct Profile;
-        /*interface*/
+
+        /**
+         * \brief Interface for all profile reporters
+         */
         class SCREEN_UTILS_EXPORT ProfilerReporter {
         public:
+            /**
+             * \brief Default constructor
+             */
             ProfilerReporter();
+
+            /**
+             * \brief Destructor
+             */
             virtual ~ProfilerReporter();
-            virtual void report(Profile* profile) = 0;
+
+            /**
+             * \brief Log profile data
+             *
+             * \param[in] iProfile the profile data to log
+             */
+            virtual void report(Profile* iProfile) = 0;
         };
 
+        /**
+         * \brief Standard output profile reporter
+         *
+         * Writes the profile data into the console
+         */
         class SCREEN_UTILS_EXPORT CoutProfilerReporter : public ProfilerReporter {
         public:
-            void report(Profile* profile);
+            /**
+             * \brief Log profile data
+             *
+             * \param[in] iProfile the profile data to log
+             */
+            void report(Profile* iProfile);
         };
         
+        /**
+         * \brief File output profile reporter
+         *
+         * Writes the profile data into a given file
+         */
         class SCREEN_UTILS_EXPORT FileProfilerReporter : public ProfilerReporter {
         public:
-        	FileProfilerReporter(const char* file);
-            void report(Profile* profile);
+            FileProfilerReporter(const char* iFile);
+            /**
+             * \brief Log profile data
+             *
+             * \param[in] iProfile the profile data to log
+             */
+            void report(Profile* iProfile);
         private:
-        	const char* file;
+            const char* _file; ///< File path
         };
     }
 }
