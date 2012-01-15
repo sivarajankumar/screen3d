98b71df6e6d21f24343a501a88702dc1a6884e37
thoratou <thoratou@df6f4744-0210-11df-9fb0-db4daf2f83fa>
2012-01-15 12:44:45 +0000

    Doxygen and code rules for logger reporters

--- Screen/Utils/include/screen/utils/LoggerReporter.h
+++ Screen/Utils/include/screen/utils/LoggerReporter.h
@@ -18,6 +18,12 @@
  * Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA, or go to   *
  * http://www.gnu.org/copyleft/lesser.txt.                                   *
  *****************************************************************************/
+/**
+ * \file screen/utils/LoggerReporter.h
+ * \brief Screen/Utils log message output handling header file
+ * \author
+ *
+ */
 
 #ifndef SCREEN_LOGGER_REPORTER_H
 #define SCREEN_LOGGER_REPORTER_H
@@ -27,53 +33,198 @@
 #include <string>
 #include <fstream>
 
+/**
+ * Namespace for all screen classes
+ */
 namespace screen {
+    /**
+     * Namespace for all utility classes
+     */
     namespace utils {
+
+        /**
+         * \brief Interface for all log reporters
+         */
         class SCREEN_UTILS_EXPORT LoggerReporter {
         public:
+            /**
+             * \brief Default constructor
+             */
             LoggerReporter();
-            virtual ~LoggerReporter();
-            virtual void debug(const std::string& log) = 0;
-            virtual void info(const std::string& log) = 0;
-            virtual void warning(const std::string& log) = 0;
-            virtual void error(const std::string& log) = 0;
-            virtual void call(CallPosition pos,
-            				  CallType type,
-            				  const char* className,
-            				  const std::string& functionName,
-            				  const std::string& address) = 0;
+
+            /**
+             * \brief Destructor
+             */
+            virtual ~LoggerReporter() = 0;
+
+            /**
+             * \brief Log a debug message
+             *
+             * \param[in] iLog the debug message
+             */
+            virtual void debug(const std::string& iLog) = 0;
+
+            /**
+             * \brief Log an info message
+             *
+             * \param[in] iLog the info message
+             */
+            virtual void info(const std::string& iLog) = 0;
+
+            /**
+             * \brief Log a warning message
+             *
+             * \param[in] iLog the warning message
+             */
+            virtual void warning(const std::string& iLog) = 0;
+
+            /**
+             * \brief Log an error message
+             *
+             * \param[in] iLog the error message
+             */
+            virtual void error(const std::string& iLog) = 0;
+
+            /**
+             * \brief Log a call message
+             *
+             * \param[in] iPos The call position (begin or end)
+             * \param[in] iType The call type
+             * \param[in] iClassName The name of the class
+             * \param[in] iFunctionName The function or method name
+             * \param[in] iAddress The address of the traced instance
+             */
+            virtual void call(CallPosition iPos,
+                      CallType iType,
+                      const char* iClassName,
+                      const std::string& iFunctionName,
+                      const std::string& iAddress) = 0;
         };
 
+        /**
+         * \brief Standard output reporter
+         *
+         * Writes the log messages into the console
+         */
         class SCREEN_UTILS_EXPORT CoutLoggerReporter : public LoggerReporter {
         public:
+            /**
+             * \brief Default constructor
+             */
             CoutLoggerReporter();
+
+            /**
+             * \brief Destructor
+             */
             ~CoutLoggerReporter();
-            void debug(const std::string& log);
-            void info(const std::string& log);
-            void warning(const std::string& log);
-            void error(const std::string& log);
-            void call(CallPosition pos,
-            		  CallType type,
-            		  const char* className,
-            		  const std::string& functionName,
-            		  const std::string& address);
+
+            /**
+             * \brief Log a debug message in standard output stream
+             *
+             * \param[in] iLog the debug message
+             */
+            void debug(const std::string& iLog);
+
+            /**
+             * \brief Log an info message in standard output stream
+             *
+             * \param[in] iLog the info message
+             */
+            void info(const std::string& iLog);
+
+            /**
+             * \brief Log a warning message in standard output stream
+             *
+             * \param[in] iLog the warning message
+             */
+            void warning(const std::string& iLog);
+
+            /**
+             * \brief Log an error message in standard output stream
+             *
+             * \param[in] iLog the error message
+             */
+            void error(const std::string& iLog);
+
+            /**
+             * \brief Log a call message in standard output stream
+             *
+             * \param[in] iPos The call position (begin or end)
+             * \param[in] iType The call type
+             * \param[in] iClassName The name of the class
+             * \param[in] iFunctionName The function or method name
+             * \param[in] iAddress The address of the traced instance
+             */
+            void call(CallPosition iPos,
+                      CallType iType,
+                      const char* iClassName,
+                      const std::string& iFunctionName,
+                      const std::string& iAddress);
         };
         
+        /**
+         * \brief File output reporter
+         *
+         * Writes the log messages into a given file
+         */
         class SCREEN_UTILS_EXPORT FileLoggerReporter : public LoggerReporter {
         public:
-        	FileLoggerReporter(const char* file);
+            /**
+             * \brief Constructor with output file path
+             *
+             * \param[in] iFile complete file path
+             */
+            FileLoggerReporter(const char* iFile);
+
+            /**
+             * \brief Destructor
+             */
             ~FileLoggerReporter();
-            void debug(const std::string& log);
-            void info(const std::string& log);
-            void warning(const std::string& log);
-            void error(const std::string& log);
-            void call(CallPosition pos,
-            		  CallType type,
-            		  const char* className,
-            		  const std::string& functionName,
-            		  const std::string& address);
+
+            /**
+             * \brief Log a debug message in file output stream
+             *
+             * \param[in] iLog the debug message
+             */
+            void debug(const std::string& iLog);
+
+            /**
+             * \brief Log an info message in file output stream
+             *
+             * \param[in] iLog the info message
+             */
+            void info(const std::string& iLog);
+
+            /**
+             * \brief Log a warning message in file output stream
+             *
+             * \param[in] iLog the warning message
+             */
+            void warning(const std::string& iLog);
+
+            /**
+             * \brief Log an error message in file output stream
+             *
+             * \param[in] iLog the error message
+             */
+            void error(const std::string& iLog);
+
+            /**
+             * \brief Log a call message in file output stream
+             *
+             * \param[in] iPos The call position (begin or end)
+             * \param[in] iType The call type
+             * \param[in] iClassName The name of the class
+             * \param[in] iFunctionName The function or method name
+             * \param[in] iAddress The address of the traced instance
+             */
+            void call(CallPosition iPos,
+                      CallType iType,
+                      const char* iClassName,
+                      const std::string& iFunctionName,
+                      const std::string& iAddress);
         private:
-        	std::ofstream fileStream;
+            std::ofstream _fileStream; ///< output file stream
         };
     }
 }
