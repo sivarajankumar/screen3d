baa20ae12ed7a18c2754d40ea578cd457f550bec
thoratou <thoratou@df6f4744-0210-11df-9fb0-db4daf2f83fa>
2012-01-17 20:52:33 +0000

    Resource manager class migration to Doxygen and coding rules

--- trunk/Screen/Utils/include/screen/utils/ResourceManager.h
+++ trunk/Screen/Utils/include/screen/utils/ResourceManager.h
@@ -18,6 +18,12 @@
  * Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA, or go to   *
  * http://www.gnu.org/copyleft/lesser.txt.                                   *
  *****************************************************************************/
+/**
+ * \file screen/utils/ResourceManager.h
+ * \brief Screen/Utils resource manager header file
+ * \author
+ *
+ */
 
 #ifndef SCREEN_RESOURCE_MANAGER_H
 #define SCREEN_RESOURCE_MANAGER_H
@@ -27,35 +33,68 @@
 #include <screen/utils/Declaration.h>
 #include <map>
 
+/**
+ * Namespace for all screen classes
+ */
 namespace screen {
+	/**
+	 * Namespace for all utility classes
+	 */
 	namespace utils {
-		/*!  \class ResourceManager
-		 *   \brief 
-		 *   \author Ratouit Thomas
-		 *   \date 20 sept. 09
-		 */
 
+		/**
+		 * \brief Manager for all resource instances
+		 */
 		class SCREEN_UTILS_EXPORT ResourceManager : public UniqueSingleton<ResourceManager> {
 			SCREEN_DECL_CLASS(screen::utils::ResourceManager)
 			SINGLETON_DECL(UniqueSingleton,ResourceManager)
 		public:
-			template <class T> T* get(const std::string& name) const;
-			void add(const std::string& name, ResourceBase* resource);
-			void remove(const std::string& name);
+			/**
+			 * \brief Retrieve a resource from its name
+			 *
+			 * \tparam T The resource real type
+			 * \param[in] iName The name of the resource
+			 * \return The resource instance
+			 */
+			template <class T> T* get(const std::string& iName) const;
+
+			/**
+			 * \brief Register a resource and its name
+			 *
+			 * \param[in] iName The name of the resource
+			 * \param[in,out] ioResource The resource instance
+			 */
+			void add(const std::string& iName, ResourceBase* ioResource);
+
+			/**
+			 * \brief Remove a resource from its name
+			 *
+			 * \param[in] iName The name of the resource
+			 */
+			void remove(const std::string& iName);
 		private:
+			/**
+			 * \brief Default constructor
+			 */
 			ResourceManager();
+
+			/**
+			 * \brief Destructor
+			 */
 			~ResourceManager();
+
 			typedef std::map<std::string, ResourceBase*> ResourceMap;
-			ResourceMap resourceMap;
+
+			ResourceMap _resourceMap; ///< Mapping between the resource name and the resource instances
 		};
 		
 		template <class T>
-		inline T* ResourceManager::get(const std::string& name) const{
+		inline T* ResourceManager::get(const std::string& iName) const{
 			SCREEN_DECL_METHOD(get)
-		    ResourceMap::const_iterator i = resourceMap.find(name);
-		    if (i != resourceMap.end()){
-		        i->second->add();
-		        return dynamic_cast<T*>(i->second);
+			ResourceMap::const_iterator aIt = _resourceMap.find(iName);
+			if (aIt != _resourceMap.end()){
+				aIt->second->add();
+				return dynamic_cast<T*>(aIt->second);
 		    }
 		    else{
 		        return NULL;
